f19e9a020bd901e7a0c9cf1206485d22bc0f4ebb
macandrew6 <mr.betterandrewmac@gmail.com>
2019-08-22 02:20:56 -0700

    diagonal_difference

---
+++ diagonal_difference.js
@@ -0,0 +1,25 @@
+let arr = [
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12]
+]
+
+function diagonalDifference(arr) {
+  // Write your code here
+  let rightDiagSum = 0;
+  let leftDiagSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i === j) {
+        rightDiagSum += arr[i][j];
+      }
+      if (i === arr.length - j - 1) {
+        leftDiagSum += arr[i][j];
+      }
+
+    }
+  }
+  return rightDiagSum - leftDiagSum
+}
+
+console.log(diagonalDifference(arr))
