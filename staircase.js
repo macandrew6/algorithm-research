88f78191b35b57ecaa41b6d475ad566040860113
macandrew6 <mr.betterandrewmac@gmail.com>
2019-08-22 17:04:20 -0700

    starts staircase

---
+++ staircase.js
@@ -0,0 +1,21 @@
+/*
+Output Format:
+Print a staircase of size  using # symbols and spaces.
+
+Sample input:
+6
+
+Sample output:
+     #
+    ##
+   ###
+  ####
+ #####
+######
+*/
+
+function staircase(n) {
+
+}
+
+console.log(staircase(6));
