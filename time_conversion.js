acc40d6469e419f774cedaebdc349cf6762edad8
macandrew6 <mr.betterandrewmac@gmail.com>
2019-08-23 22:09:52 -0700

    debuggs time conversion

--- time_conversion.js
+++ time_conversion.js
@@ -4,5 +4,19 @@ Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
 Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour 
 clock. Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock.
 
+Output format:
 
+Convert and print given time in 24 hour format, where 00 <= hh <= 23
+
+Sample Input:
+
+07:05:45PM
+
+Sample Output:
+
+19:05:45
 */
+
+function timeConversion(s) {
+  
+}
