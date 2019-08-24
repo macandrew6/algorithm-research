8c195ee6ac71157d9e1bc45321ea7dd071245237
macandrew6 <mr.betterandrewmac@gmail.com>
2019-08-23 21:15:33 -0700

    starts mini_max_sum

---
+++ mini_max_sum.js
@@ -0,0 +1,26 @@
+/*
+Input Format:
+
+A single line of five space - separated integers.
+
+Output Format:
+
+Print two space-separated long integers denoting the respective minimum and 
+maximum values that can be calculated by summing exactly four of the five 
+integers. (The output can be greater than a 32 bit integer.)
+
+Sample Input:
+
+1 2 3 4 5
+
+Sample Output:
+
+10 14
+*/
+
+function miniMaxSum(input) {
+  
+}
+
+let input = [1, 2, 3, 4, 5];
+console.log(miniMaxSum(input));
