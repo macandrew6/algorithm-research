71aa4b19177e934ceba0939ac595520418499270
macandrew6 <mr.betterandrewmac@gmail.com>
2019-08-23 21:05:50 -0700

    completes staircase

--- staircase.js
+++ staircase.js
@@ -15,7 +15,13 @@ Sample output:
 */
 
 function staircase(n) {
-
+     for (let i = 0; i < n; i++) {
+          let row = '';
+          for (let j = 0; j < n - i - 1; j++) {
+               row += ' ';
+          }
+          console.log(row + '#'.repeat(i + 1));
+     }
 }
 
 console.log(staircase(6));
