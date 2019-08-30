bcde9af3b7e71290cad8beec638ecd7fda5fefae
macandrew6 <mr.betterandrewmac@gmail.com>
2019-08-29 20:20:58 -0700

    debuggs word_pattern

--- word_pattern.js
+++ word_pattern.js
@@ -25,6 +25,25 @@ You may assume pattern contains only lowercase letters, and str contains
 lowercase letters that may be separated by a single space.
 */
 
-let wordPattern = function(pattern, str) {
-
+var wordPattern = function (pattern, str) {
+  var pArr = pattern.split("");
+  var sArr = str.split(" ");
+  if (pArr.length !== sArr.length) return false;
+  var mapP2S = {};
+  var mapS2P = {};
+  for (var i = 0; i < pArr.length; ++i) {
+    if (pArr[i] in mapP2S) {
+      if (sArr[i] !== mapP2S[pArr[i]]) return false;
+    }
+    else {
+      mapP2S[pArr[i]] = sArr[i];
+    }
+    if (sArr[i] in mapS2P) {
+      if (pArr[i] !== mapS2P[sArr[i]]) return false;
+    }
+    else {
+      mapS2P[sArr[i]] = pArr[i];
+    }
+  }
+  return true;
 };
