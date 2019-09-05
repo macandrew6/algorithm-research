7a651f03aa0f9a070817293ba5165903528f65e6
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-05 00:30:51 -0700

    updates pseudo code for product except self

--- product_except_self.js
+++ product_except_self.js
@@ -14,6 +14,15 @@ count as extra space for the purpose of space complexity analysis.)
 */
 
 function productExceptSelf(nums) {
+  // 1. Since we can't use divide, we calculate two pars, the left product and 
+  //    the right product, separate by the current index i
+  // 2. We first calculate the left product, store them to res array. so res[i]
+  //    means the left product for the current number i. Note: res[0] = 1 (To 
+  //    future me: Figure this out yourself pls)
+  // 3. We can calculate from right side fo the array, calculate the right 
+  //    produce of the current index i, store them to a variable right;
+  // 4. res[i] = res[i] * right;
+  // 5. update right = right * nums[i]
   let len = nums.length;
   let leftProduct = [];
   leftProduct[0] = 1;
