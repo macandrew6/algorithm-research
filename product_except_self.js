d2c4732b5d7098087aa0a5c94a31aad4961c2a56
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-04 22:43:07 -0700

    more efficient solution for product except self

--- product_except_self.js
+++ product_except_self.js
@@ -14,10 +14,32 @@ count as extra space for the purpose of space complexity analysis.)
 */
 
 function productExceptSelf(nums) {
+  let len = nums.length;
+  let leftProduct = [];
+  leftProduct[0] = 1;
+  for (let i = 1; i < len; i++) { // i = 4
+    leftProduct[i] = leftProduct[i - 1] * nums[i - 1]; // 6 * 4 = 24
+  }
 
+  let result = [];
+  let rightProduct = 1;
+  for (let i = len - 1; i >= 0; i--) { // i = 0
+    result[i] = leftProduct[i] * rightProduct; // 1 * 24 = 24
+    rightProduct *= nums[i];
+  }
+  return result;
 }
 
 productExceptSelf([1, 2, 3, 4]);
+
+/* 
+nums = [1, 2, 3, 4]
+len = 4
+leftProduct = [1, 1, 2, 6]
+result = [24, 12, 8, 6]
+rightProduce = 24
+
+*/
 
 /*
 let result = [];
