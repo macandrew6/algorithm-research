c3199bc9041bd83403065c5d9178ecc95e99028b
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-05 17:15:33 -0700

    strategizes solution

--- islands.js
+++ islands.js
@@ -1,11 +1,19 @@
+/*
 // the 1's are land the 0's are sea, return the count of the islands on the grid
 // Islands are created only by connections from the top, right, bottom, and left
 
-// [[1, 1, 0, 0],
-//  [1, 0, 1, 0],
-//  [1, 0, 0, 1]]
+Input:
 
-// output: 3
+[[1, 1, 0, 0],
+ [1, 0, 1, 0],
+ [1, 0, 0, 1]]
+
+[[a, b, 0, 0],
+ [c, 0, e, 0],
+ [d, 0, 0, f]]
+// Output: 
+  3
+*/
 
 function didSinkIsland(arr2d, row, col) {
   if (arr2d[row][col] === 1) {
