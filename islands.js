2b556565a78bbc9406cdc53c6d29c80970aac8ec
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-05 17:12:54 -0700

    pseudo codes islands

--- islands.js
+++ islands.js
@@ -7,3 +7,53 @@
 
 // output: 3
 
+function didSinkIsland(arr2d, row, col) {
+  if (arr2d[row][col] === 1) {
+    const sinkSoon = [[row, col]];
+    while (sinkSoon.length > 0) {
+      let currLand = sinkSoon.pop();
+      let nextToThis = nextTo(arr2d, currLand[0], currLand[1]);
+      nextToThis = nextToThis.filter(
+        ([ntRow, ntCol]) => arr2d[ntRow][ntCol] === 1
+      );
+      nextToThis.forEach(ssSpot => {
+        sinkSoon.push(ssSpot);
+      });
+      arr2d[currLand[0]][currLand[1]] = 0;
+    }
+    return true;
+  }
+  return false;
+}
+
+function nextTo(arr2d, row, col) {
+  const answer = [];
+  if (!(row - 1 < 0) && !(row - 1 >= arr2d.length)) {
+    const up = [row - 1, col];
+    answer.push(up);
+  }
+  if (!(row + 1 < 0) && !(row + 1 >= arr2d.length)) {
+    const down = [row + 1, col];
+    answer.push(down);
+  }
+  if (!(col - 1 < 0) && !(col - 1 >= arr2d[0].length)) {
+    const left = [row, col - 1];
+    answer.push(left);
+  }
+  if (!(col + 1 < 0) && !(col + 1 >= arr2d[0].length)) {
+    const right = [row, col + 1];
+    answer.push(right);
+  }
+  return answer;
+}
+
+const i = [
+  [1, 1, 0, 0], 
+  [1, 0, 1, 0], 
+  [1, 0, 0, 1]
+];
+
+console.log(didSinkIsland(i, 0, 0));
+console.log(i[0]);
+console.log(i[1]);
+console.log(i[2]);
