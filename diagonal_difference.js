5ae7bbb7bef4d84deb2bf12fb0847fa670494caa
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-06 12:09:16 -0700

    tests diagonal diffs

--- diagonal_difference.js
+++ diagonal_difference.js
@@ -2,6 +2,7 @@
 
 function diagonalDifference(arr) {
   // Write your code here
+  debugger;
   let rightDiagSum = 0;
   let leftDiagSum = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -17,6 +18,14 @@ function diagonalDifference(arr) {
   }
   return rightDiagSum - leftDiagSum;
 }
+
+/*
+i = 2; j = 2;
+arr.length - 2 - 1 = 3 - 2 - 1 = 0
+rightSum = 11 + 5 + -12 = 4
+leftSum = 4 + 5 + 10 = 19
+output = -15
+*/
 
 let arr = [
   [11, 2, 4],
