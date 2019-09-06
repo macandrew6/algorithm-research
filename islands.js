dcc3347a35045c83c3c9a9c9e691765e379805a9
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-05 17:10:38 -0700

    starts islands

---
+++ islands.js
@@ -0,0 +1,9 @@
+// the 1's are land the 0's are sea, return the count of the islands on the grid
+// Islands are created only by connections from the top, right, bottom, and left
+
+// [[1, 1, 0, 0],
+//  [1, 0, 1, 0],
+//  [1, 0, 0, 1]]
+
+// output: 3
+
