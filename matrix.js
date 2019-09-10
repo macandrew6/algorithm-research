cdb1d0adab49a416b2bf082c2469df1ad3587e69
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-09 20:57:09 -0700

    rewriting solution using bfs data structure

--- matrix.js
+++ matrix.js
@@ -36,77 +36,68 @@ There are at least one 0 in the given matrix.
 The cells are adjacent in only four directions: up, down, left and right.
 */
 
-/*
-return to redo this solution
+
 let checkForZero = function(matrix, row, col) {
-  // returns the steps taken to the closest 0
-  // increment the step count each time we check all four sides and there are no
-  // 1's
-  // increments x and y values and decrement x and y values by 1
-  // check again all values left right up and down from the coordinate 
-  // incremented by 1
-  let steps = 0;
+  // returns all the neighboring coordinates to the current element
+  let neighbors = [];
   if (!(row - 1 < 0) && !(row - 1 >= matrix.length)) {
     let up = [row - 1, col];
+    neighbors.push(up);
   }
   if (!(row + 1 < 0) && !(row + 1 >= matrix.length)) {
     let down = [row + 1, col];
+    neighbors.push(down);
   }
   if (!(col - 1 < 0) && !(col - 1 >= matrix[0].length)) {
     let left = [row, col - 1];
+    neighbors.push(left);
   }
   if (!(col + 1 < 0) && !(col + 1 >= matrix[0].length)) {
     let right = [row, col + 1];
+    neighbors.push(right);
   }
 
-  return steps;
+  return neighbors;
 };
 
 let updateMatrix = function(matrix) {
-  // iterate through the grid
-  // find the 1's
-  // when you find the 1st 1
-  // init a variable counter
-  // we check the up down left and right coordinates
-    // if one of those coordinates is a 0
-    // counter += 1
-    // 1st 1's coordinate is equal to the counter
-    // else we check the coordinates again 1 step further
+  // use a bfs searching algorithm
 
 };
-*/
 
-var updateMatrix = function (matrix) {
-  let bfs = (row, col) => {
-    let queue = [{ row, col }];
-    let step = 0;
-    while (queue.length) {
-      let len = queue.length;
-      while (len) {
-        let { row, col } = queue.shift();
-        if (row > 0) queue.push({ row: row - 1, col });
-        if (col > 0) queue.push({ row, col: col - 1 });
-        if (row < matrix.length - 1) queue.push({ row: row + 1, col });
-        if (col < matrix[0].length - 1) queue.push({ row, col: col + 1 });
 
-        if (matrix[row][col] === 0) return step;
-        len--;
-      }
-      step++;
-    }
-  };
+// var updateMatrix = function (matrix) {
+//   debugger;
+//   let bfs = (row, col) => {
+//     let queue = [{ row, col }];
+//     let step = 0;
+//     while (queue.length) {
+//       let len = queue.length;
+//       while (len) {
+//         let { row, col } = queue.shift();
+//         if (row > 0) queue.push({ row: row - 1, col });
+//         if (col > 0) queue.push({ row, col: col - 1 });
+//         if (row < matrix.length - 1) queue.push({ row: row + 1, col });
+//         if (col < matrix[0].length - 1) queue.push({ row, col: col + 1 });
 
-  return matrix.map((r, rIdx) => {
-    return r.map((c, cIdx) => {
-      return matrix[rIdx][cIdx] === 0 ? 0 : bfs(rIdx, cIdx);
-    });
-  });
-};
+//         if (matrix[row][col] === 0) return step;
+//         len--;
+//       }
+//       step++;
+//     }
+//   };
+
+//   return matrix.map((r, rIdx) => {
+//     return r.map((c, cIdx) => {
+//       return matrix[rIdx][cIdx] === 0 ? 0 : bfs(rIdx, cIdx);
+//     });
+//   });
+// };
 
 let matrix = [
   [0, 0, 0],
   [0, 1, 0],
-  [0, 0, 0]
+  [1, 1, 1]
 ];
 
 console.log(updateMatrix(matrix));
