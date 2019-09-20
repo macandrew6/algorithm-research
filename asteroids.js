e5e7de57ced5e3c56774d14e7d4b2c55934f22b9
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-19 20:56:12 -0700

    starts new implementation

--- asteroids.js
+++ asteroids.js
@@ -24,39 +24,47 @@ if the stack is empty push the element into the stack
                                       stack, (do not increase the array index)
 
 */
-
-var asteroidCollision = function(asteroids) {
-  let collided = collision(asteroids);
-
-  if (JSON.stringify(collided) === JSON.stringify(collision(collided))) {
-    console.log("finished");
-    return collided;
-  } else {
-    console.log("running");
-    return asteroidCollision(collided);
+let asteroidCollision = function(asteroids) {
+  // new implementation
+  let stack = [];
+  for (let i = 0; i < asteroids.length; i++) {
+    if (!stack.length) stack.push(asteroids[i]);
   }
 };
 
-let collision = asteroids => {
-  if (!asteroids.length) return [];
-  let stack = [asteroids[0]];
-  for (let i = 1; i < asteroids.length; i++) {
-    let top = stack[stack.length - 1];
-    let curr = asteroids[i];
-    if (Math.sign(top) === Math.sign(curr)) {
-      stack.push(curr);
-    } else {
-      if (Math.abs(top) > Math.abs(curr)) {
-        continue;
-      } else if (Math.abs(top) < Math.abs(curr)) {
-        stack.pop();
-        stack.push(curr);
-      } else if (Math.abs(top) === Math.abs(curr)) {
-        stack.pop();
-      }
-    }
-  }
-  return stack;
-};
+
+// var asteroidCollision = function(asteroids) {
+//   let collided = collision(asteroids);
+
+//   if (JSON.stringify(collided) === JSON.stringify(collision(collided))) {
+//     console.log("finished");
+//     return collided;
+//   } else {
+//     console.log("running");
+//     return asteroidCollision(collided);
+//   }
+// };
+
+// let collision = asteroids => {
+//   if (!asteroids.length) return [];
+//   let stack = [asteroids[0]];
+//   for (let i = 1; i < asteroids.length; i++) {
+//     let top = stack[stack.length - 1];
+//     let curr = asteroids[i];
+//     if (Math.sign(top) === Math.sign(curr)) {
+//       stack.push(curr);
+//     } else {
+//       if (Math.abs(top) > Math.abs(curr)) {
+//         continue;
+//       } else if (Math.abs(top) < Math.abs(curr)) {
+//         stack.pop();
+//         stack.push(curr);
+//       } else if (Math.abs(top) === Math.abs(curr)) {
+//         stack.pop();
+//       }
+//     }
+//   }
+//   return stack;
+// };
 
 console.log(collision([]));
