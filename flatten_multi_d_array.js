eefa28c861beb22bffac053927bc14a073927a14
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-25 00:21:15 -0700

    solves flatten

--- flatten_multi_d_array.js
+++ flatten_multi_d_array.js
@@ -1,6 +1,18 @@
-let arr1 = [1, 2, 3, [4, 5, [6, 7]]];
-arr1.flat();
+
 
 Array.prototype.flat = function() {
   let result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (Array.isArray(this[i])) {
+      result = result.concat(this[i].flat());
+    } else {
+      result.push(this[i]);
+    }
+  }
+
+  return result;
 };
+
+let arr1 = [1, 2, 3, [4, 5, [6, 7]]];
+console.log(arr1.flat());
