aa021369e1d527d3f99099fad6cc1028d61d6c5e
macandrew6 <mr.betterandrewmac@gmail.com>
2019-10-02 11:53:55 -0700

    adds generate matrix

--- orange_rotting.js
+++ orange_rotting.js
@@ -16,8 +16,21 @@ Input: [[2,1,1],[0,1,1],[1,0,1]]
 Output: -1
 Explanation:  The orange in the bottom left corner (row 2, column 0) is never 
 rotten, because rotting only happens 4-directionally.
+
+Example 3:
+
+Input: [[0,2]]
+Output: 0
+Explanation:  Since there are already no fresh oranges at minute 0, the answer 
+is just 0.
+
+Note:
+
+1 <= grid.length <= 10
+1 <= grid[0].length <= 10
+grid[i][j] is only 0, 1, or 2.
 */
 
-function orangeRotting() {
+function orangeRotting(grid) {
 
 }
