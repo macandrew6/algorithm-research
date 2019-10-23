55c7174fd5a36d32e91ed35680dea804567273a1
macandrew6 <mr.betterandrewmac@gmail.com>
2019-10-23 16:47:08 -0700

    removes non-working solution

--- asteroids.js
+++ asteroids.js
@@ -54,38 +54,8 @@ let asteroidCollision = function(asteroids) {
 };
 
 
-// var asteroidCollision = function(asteroids) {
-//   let collided = collision(asteroids);
+let asteroids1 = [-1, 2, -1]; // => [-1, 2]
+let asteroids2 = [4, -3]; // => [4]
+let asteroids3 = [1, 2, -3]; // => [-3]
 
-//   if (JSON.stringify(collided) === JSON.stringify(collision(collided))) {
-//     console.log("finished");
-//     return collided;
-//   } else {
-//     console.log("running");
-//     return asteroidCollision(collided);
-//   }
-// };
-
-// let collision = asteroids => {
-//   if (!asteroids.length) return [];
-//   let stack = [asteroids[0]];
-//   for (let i = 1; i < asteroids.length; i++) {
-//     let top = stack[stack.length - 1];
-//     let curr = asteroids[i];
-//     if (Math.sign(top) === Math.sign(curr)) {
-//       stack.push(curr);
-//     } else {
-//       if (Math.abs(top) > Math.abs(curr)) {
-//         continue;
-//       } else if (Math.abs(top) < Math.abs(curr)) {
-//         stack.pop();
-//         stack.push(curr);
-//       } else if (Math.abs(top) === Math.abs(curr)) {
-//         stack.pop();
-//       }
-//     }
-//   }
-//   return stack;
-// };
-
-console.log(collision([]));
+console.log(asteroidCollision(asteroids1));
