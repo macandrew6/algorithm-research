9a387820e8cbd5bb84fc815ce29d4c9f7a3c6105
macandrew6 <mr.betterandrewmac@gmail.com>
2019-11-21 15:11:12 -0800

    brute force two sum solution

---
+++ two_sum.js
@@ -0,0 +1,26 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add
+ up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not
+use the same element twice.
+
+  Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+// brute force O(n^2) time O(1) space solution
+
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j < nums.length; j++) {
+      if (i !== j && nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
